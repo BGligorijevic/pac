988c6b891903cc5c73ed7c3d84a3183181a169e5
BGligorijevic <boris.gligorijevic@prodyna.com>
2016-02-27 00:28:45 +0100

    KwPcWN1X: MySql database and user.

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
+
+# install mysql
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get install -y vim curl python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
+
+# allow external access
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+
+# root is the god
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+# create app user and table
+mysql -u root -proot -e "CREATE DATABASE IF NOT EXISTS VOTES; 
+GRANT USAGE ON *.* TO 'voteapp'@'%' IDENTIFIED BY 'voteapp';
+GRANT SELECT, INSERT, UPDATE, DELETE ON VOTES.* TO 'voteapp'@'%';
+FLUSH PRIVILEGES;"
+
 sudo /etc/init.d/mysql restart
