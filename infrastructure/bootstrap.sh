94d4de97a98be2438ac688f4d74c6952a0f6a1f5
BGligorijevic <boris.gligorijevic@prodyna.com>
2016-02-26 23:31:03 +0100

    ynXRoj5o: Changed scripts so mysql could be accessed as if on host mashine.

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-apt-get update
-apt-get install -y mysql-server
+sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get update
+sudo apt-get -y install mysql-server
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+sudo /etc/init.d/mysql restart
