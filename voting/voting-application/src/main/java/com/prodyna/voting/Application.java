3a0a9385f2ab75594318bec64b7a0135347d6c3e
Boris Gligorijevic <boris.gligorijevic@prodyna.com>
2016-05-28 23:00:49 +0200

    Added CORS support for local client server.

--- voting/voting-application/src/main/java/com/prodyna/voting/Application.java
+++ voting/voting-application/src/main/java/com/prodyna/voting/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -26,5 +29,18 @@ public class Application {
         registrationBean.addUrlPatterns(SecurityFilter.SECURED_API_PATH);
 
         return registrationBean;
+    }
+
+    /**
+     * Adds CORS allowed origin for local client-side development server which we can trust.
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
