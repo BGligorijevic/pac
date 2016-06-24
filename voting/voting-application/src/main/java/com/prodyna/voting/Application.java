2bfbf87350482711548788fa103c71dd20fae59e
Boris Gligorijevic <boris.gligorijevic@prodyna.com>
2016-06-25 00:24:50 +0200

    added cors entry for localhost

--- voting/voting-application/src/main/java/com/prodyna/voting/Application.java
+++ voting/voting-application/src/main/java/com/prodyna/voting/Application.java
@@ -39,7 +39,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedHeaders("*").allowedMethods("*");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost").allowedHeaders("*").allowedMethods("*");
             }
         };
     }
