97c1f554f162dba7f684ee75ba8ecd21686c7b43
Boris Gligorijevic <boris.gligorijevic@prodyna.com>
2016-07-06 21:09:16 +0200

    Changed script runner plugin

--- voting/voting-application/src/main/resources/scripts/build.bat
+++ voting/voting-application/src/main/resources/scripts/build.bat
@@ -1,1 +1,2 @@
-cd ..\..\..\pac-front-end && npm install && npm run clean && npm run build
+echo "Executing build.bat script..."
+cd ..\..\dev-infrastructure && vagrant up && cd ..\..\pac-front-end && npm install && npm run clean && npm run build
