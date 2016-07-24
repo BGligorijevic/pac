3b724e5b6e7de8610cdb0c37fd7a12ab5bda77c0
Boris Gligorijevic <boris.gligorijevic@prodyna.com>
2016-07-24 19:58:15 +0200

    Deployment scripts and docs improved

--- voting/voting-application/src/main/resources/scripts/start.bat
+++ voting/voting-application/src/main/resources/scripts/start.bat
@@ -1,2 +1,2 @@
 @echo off
-START cmd.exe /k "cd ../back-end && java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888 -javaagent:voting-application-1.0-SNAPSHOT.jar -jar voting-application-1.0-SNAPSHOT.jar --voting.dev.mode=true --voting.app.secret.key=sample_dev_key"
+START cmd.exe /k "cd ../back-end && java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888 -javaagent:voting-application-1.0-SNAPSHOT.jar -jar voting-application-1.0-SNAPSHOT.jar
